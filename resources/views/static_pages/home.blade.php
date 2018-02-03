5ab37313c5c1b3e559d1a6b66b850d7f5f53d4a5
Gary_Leung <garyliangljl@outlook.com>
2018-02-03 16:51:38 +0000

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
+
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            You can see the demo project homepage <a href="https://laravel-china.org/courses/laravel-essential-training-5.1">Laravel 入门教程</a>.
+        </p>
+        <p>
+            Everything will start here.
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">Sign up now</a>
+        </p>
+    </div>
 @stop
